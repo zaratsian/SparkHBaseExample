f971b9f69107a2d191ac05c7f606c0a58617d787
Randy Gelhausen <rgelhau@gmail.com>
2016-08-12 14:40:17 -0400

    Externalized job to a props file

--- src/main/scala/com/github/zaratsian/SparkHBase/SparkHBase.scala
+++ src/main/scala/com/github/zaratsian/SparkHBase/SparkHBase.scala
@@ -27,8 +27,9 @@ import org.apache.hadoop.fs._
 object SparkHBase{
 
   def main(args: Array[String]) {
+    val props = getProps(args(0))
+
     val config = ConfigFactory.load()
-    val hbaseRootDir =  "/apps/hbase"
     val sparkConf = new SparkConf()
       .setAppName("testnsnap")
       //.setMaster(config.getString("spark.app.master"))
@@ -41,14 +42,17 @@ object SparkHBase{
     println("Creating hbase configuration")
     val conf = HBaseConfiguration.create()
 
-    conf.set("hbase.rootdir", hbaseRootDir)
-    conf.set("hbase.zookeeper.quorum",  "localhost:2181:/hbase-unsecure")
+    conf.set("hbase.rootdir", props.getOrElse("hbase.rootdir", "/apps/hbase"))
+    conf.set("hbase.zookeeper.quorum",  props.getOrElse("hbase.zookeeper.quorum", "localhost:2181:/hbase-unsecure"))
+
     val scan = new Scan
     conf.set(TableInputFormat.SCAN, convertScanToString(scan))
 
     val job = Job.getInstance(conf)
 
-    TableSnapshotInputFormat.setInput(job, "customer_info_ss2", new Path("/tmp/.hbase-snapshot/customer_info_ss2"))
+    val path = new Path(props.getOrElse("hbase.snapshot.path", "/apps/hbase/.hbase-snapshot"))
+    val snapName = props.getOrElse("hbase.snapshot.name", "test")
+    TableSnapshotInputFormat.setInput(job, snapName, path)
 
     val hBaseRDD = sc.newAPIHadoopRDD(job.getConfiguration, classOf[TableSnapshotInputFormat],
       classOf[org.apache.hadoop.hbase.io.ImmutableBytesWritable],
@@ -64,4 +68,14 @@ object SparkHBase{
       Base64.encodeBytes(proto.toByteArray());
   }
 
+  def getArrayProp(props: => HashMap[String,String], prop: => String): Array[String] = {
+    return props.getOrElse(prop, "").split(",").filter(x => !x.equals(""))
+  }
+
+  def getProps(file: => String): HashMap[String,String] = {
+    var props = new HashMap[String,String]
+    val lines = fromFile(file).getLines
+    lines.foreach(x => if (x contains "=") props.put(x.split("=")(0), if (x.split("=").size > 1) x.split("=")(1) else null))
+    props
+  }
 }
