3057e1b938632b04743e1aa894d17c9985fa435b
Daniel Zaratsian <dzaratsian@HW13502.local>
2016-08-17 23:50:04 -0400

    Updated with RDD and timestamp

--- src/main/scala/com/github/zaratsian/SparkHBase/SparkHBase.scala
+++ src/main/scala/com/github/zaratsian/SparkHBase/SparkHBase.scala
@@ -18,28 +18,39 @@ import java.lang.String
 import org.apache.hadoop.conf._
 import org.apache.hadoop.fs._
 
+import org.apache.hadoop.hbase.util.Bytes
+
+import scala.collection.JavaConverters._
+import org.apache.hadoop.hbase.CellUtil
+import org.apache.hadoop.hbase.KeyValue.Type
+
+
+import java.text.SimpleDateFormat
+import java.util.Arrays
+import java.util.Date
+
+
 
 object SparkHBase{
 
   def main(args: Array[String]) {
-    val props = getProps(args(0))
 
-    val sparkConf = new SparkConf().setAppName(props.getOrElse("spark.appName", "test"))
+    val sparkConf = new SparkConf().setAppName("testsnap")
     val sc = new SparkContext(sparkConf)
 
     println("Creating hbase configuration")
     val conf = HBaseConfiguration.create()
 
-    conf.set("hbase.rootdir", props.getOrElse("hbase.rootdir", "/apps/hbase/data"))
-    conf.set("hbase.zookeeper.quorum",  props.getOrElse("hbase.zookeeper.quorum", "localhost:2181:/hbase-unsecure"))
+    conf.set("hbase.rootdir", "/apps/hbase/data")
+    conf.set("hbase.zookeeper.quorum", "localhost:2181:/hbase-unsecure")
 
-    val scan = new Scan
+    val scan = new Scan 
     conf.set(TableInputFormat.SCAN, convertScanToString(scan))
 
     val job = Job.getInstance(conf)
 
-    val path = new Path(props.getOrElse("hbase.snapshot.path", "/apps/hbase/data/.hbase-snapshot"))
-    val snapName = props.getOrElse("hbase.snapshot.name", "test")
+    val path = new Path("/user/hbase")
+    val snapName = "customer_info_ss"
     TableSnapshotInputFormat.setInput(job, snapName, path)
 
     val hBaseRDD = sc.newAPIHadoopRDD(job.getConfiguration, classOf[TableSnapshotInputFormat],
@@ -48,8 +59,51 @@ object SparkHBase{
 
     println(hBaseRDD.count())
 
-    System.exit(0)
-  }
+    
+    hBaseRDD.foreach(e=> ( println("%s".format( Bytes.toString(e._1.get()) ) ) ))
+    
+
+    val keyValue = hBaseRDD.map(x => x._2).map(_.list)
+
+    val zout = keyValue.flatMap(x =>  x.asScala.map(cell =>
+        "columnFamily=%s,qualifier=%s,timestamp=%s,type=%s,value=%s".format(
+          Bytes.toStringBinary(CellUtil.cloneFamily(cell)),
+          Bytes.toStringBinary(CellUtil.cloneQualifier(cell)),
+          cell.getTimestamp,
+          Type.codeToType(cell.getTypeByte),
+          Bytes.toStringBinary(CellUtil.cloneValue(cell))
+        )
+      )
+    )
+
+
+    zout.collect().foreach(x => println(x))
+
+
+
+   
+val date_string = "2016-08-12 12:59:59:800"
+
+val dt_num = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss:SSS").parse(date_string).getTime()
+
+val results = zout.map(x => x.split(',')).filter(x => {var temp = x(2).replace("timestamp=","").toLong; temp>dt_num})
+
+results.map(cell =>
+        "%s,%s,%s,%s,%s".format(
+          cell(0).toString,
+          cell(1).toString,
+          cell(2).toString,
+          cell(3).toString,
+          cell(4).toString
+        )
+    ).collect().foreach(x => println(x))
+
+ 
+
+    sc.stop()
+
+    
+  }  
 
   def convertScanToString(scan : Scan) = {
       val proto = ProtobufUtil.toScan(scan);
@@ -66,4 +120,6 @@ object SparkHBase{
     lines.foreach(x => if (x contains "=") props.put(x.split("=")(0), if (x.split("=").size > 1) x.split("=")(1) else null))
     props
   }
+
+
 }
